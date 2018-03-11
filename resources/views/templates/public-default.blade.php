5de8606a8f99d5cf57b20ea048da5b09cd366f13
Roméo GBONGBON <g.meo@outlook.com>
2018-03-11 09:58:41 +0100

    first commité

--- resources/views/templates/public-default.blade.php
+++ resources/views/templates/public-default.blade.php
@@ -14,7 +14,7 @@
 
 <body>
     @include('partials.header')
-    @yield('content')
+        @yield('content')
     @include('partials.footer')
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>

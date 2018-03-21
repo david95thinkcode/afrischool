1f8c8f112f69f2d17b609bddfe69e22fb91c6030
David H <david95thinkcode@gmail.com>
2018-03-21 09:52:51 +0100

    [ Addition ] Forms & HTML - Laravel collective

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -18,6 +18,7 @@ class ComposerStaticInit6156c0f747ab86bed04a46cc77e88f7f
         '4a1f389d6ce373bda9e57857d3b61c84' => __DIR__ . '/..' . '/barryvdh/laravel-debugbar/src/helpers.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
+        'f18cc91337d49233e5754e93f3ed9ec3' => __DIR__ . '/..' . '/laravelcollective/html/src/helpers.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -93,6 +94,7 @@ class ComposerStaticInit6156c0f747ab86bed04a46cc77e88f7f
         'C' => 
         array (
             'Cron\\' => 5,
+            'Collective\\Html\\' => 16,
             'Carbon\\' => 7,
         ),
         'B' => 
@@ -244,6 +246,10 @@ class ComposerStaticInit6156c0f747ab86bed04a46cc77e88f7f
         array (
             0 => __DIR__ . '/..' . '/mtdowling/cron-expression/src/Cron',
         ),
+        'Collective\\Html\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laravelcollective/html/src',
+        ),
         'Carbon\\' => 
         array (
             0 => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon',
@@ -295,12 +301,14 @@ class ComposerStaticInit6156c0f747ab86bed04a46cc77e88f7f
         'App\\Classe' => __DIR__ . '/../..' . '/app/Models/Classe.php',
         'App\\Coefficier' => __DIR__ . '/../..' . '/app/Models/Coefficier.php',
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
+        'App\\Etablissement' => __DIR__ . '/../..' . '/app/Etablissement.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
         'App\\Http\\Controllers\\Auth\\LoginController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/LoginController.php',
         'App\\Http\\Controllers\\Auth\\RegisterController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/RegisterController.php',
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\EtablissementController' => __DIR__ . '/../..' . '/app/Http/Controllers/EtablissementController.php',
         'App\\Http\\Controllers\\InscriptionController' => __DIR__ . '/../..' . '/app/Http/Controllers/InscriptionController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
@@ -355,10 +363,18 @@ class ComposerStaticInit6156c0f747ab86bed04a46cc77e88f7f
         'Carbon\\Carbon' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/Carbon.php',
         'Carbon\\CarbonInterval' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/CarbonInterval.php',
         'Carbon\\Exceptions\\InvalidDateException' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/Exceptions/InvalidDateException.php',
+        'Collective\\Html\\Componentable' => __DIR__ . '/..' . '/laravelcollective/html/src/Componentable.php',
+        'Collective\\Html\\Eloquent\\FormAccessible' => __DIR__ . '/..' . '/laravelcollective/html/src/Eloquent/FormAccessible.php',
+        'Collective\\Html\\FormBuilder' => __DIR__ . '/..' . '/laravelcollective/html/src/FormBuilder.php',
+        'Collective\\Html\\FormFacade' => __DIR__ . '/..' . '/laravelcollective/html/src/FormFacade.php',
+        'Collective\\Html\\HtmlBuilder' => __DIR__ . '/..' . '/laravelcollective/html/src/HtmlBuilder.php',
+        'Collective\\Html\\HtmlFacade' => __DIR__ . '/..' . '/laravelcollective/html/src/HtmlFacade.php',
+        'Collective\\Html\\HtmlServiceProvider' => __DIR__ . '/..' . '/laravelcollective/html/src/HtmlServiceProvider.php',
         'CreateAnneesScolairesTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_190912_create_annees_scolaires_table.php',
         'CreateClassesTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_200153_create_classes_table.php',
         'CreateCoefficierTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_202416_create_coefficier_table.php',
         'CreateElevesTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_195702_create_eleves_table.php',
+        'CreateEtablissementsTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_21_082037_create_etablissements_table.php',
         'CreateInscriptionsTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_200311_create_inscriptions_table.php',
         'CreateMatieresTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_202130_create_matieres_table.php',
         'CreateParentsTable' => __DIR__ . '/../..' . '/database/migrations/2018_03_06_195337_create_parents_table.php',

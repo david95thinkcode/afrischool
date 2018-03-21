828c65235dd4411f2abcbf5f2ce285b892c5f12e
David H <david95thinkcode@gmail.com>
2018-03-21 09:09:10 +0100

    [ Update ]

--- resources/views/templates/public-default.blade.php
+++ resources/views/templates/public-default.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     @yield('custom-css')
-    <link href="{{asset('css/ie10-viewport-bug-workaround.css')}}" rel="stylesheet"> 
+    <link href="{{ asset('css/ie10-viewport-bug-workaround.css') }}" rel="stylesheet"> 
     <!--[if IE]>
         <link href="{{asset('css/bootstrap-ie9.css')}}" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>   
@@ -30,10 +30,8 @@
     @include('partials.footer')
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
-    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
-    crossorigin="anonymous"></script>
-    <script src="{{asset('js/ie10-viewport-bug-workaround.js'')}}"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/ie10-viewport-bug-workaround.js') }}"></script>
     @yield('custom-js')
     
 </body>

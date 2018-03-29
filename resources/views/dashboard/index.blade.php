a36fcaa3071f2170ec36f7396731158fd6adcada
David H <david95thinkcode@gmail.com>
2018-03-29 21:27:01 +0100

    ...

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,8 +2,34 @@
 @section('title') Home @endsection
 @section('content')
 <div class='row'>
-    <div class="col">
+    <div class="col-sm-12">
         <h3>Accueil Dashboard</h3>
+<hr>
+        <div class="">
+            <ol>
+                <li>Etablissements
+                    <ul>
+                        <li><a href="{{ route('etablissements.create') }}">Ajouter </a></li>
+                        <li><a href="{{ route('etablissements.index') }}">Liste </a></li>
+                    </ul>
+                </li>
+                <li>Classes
+                    <ul>
+                        <li><a href="{{ route('classe.create') }}">Ajouter </a></li>
+                        <li><a href="{{ route('classe.index') }}">Liste </a></li>
+                    </ul>
+                </li>
+                <li>Professeur
+                    <ul>
+                        <li><a href="{{ route('etablissements.create') }}">Ajouter </a></li>
+                        <li><a href="{{ route('etablissements.index') }}">Liste </a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Ajouter une note</a></li>
+                <li></li>
+            </ol>
+        </div>
     </div>
+
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::prefix('dashboard')->group(function () {
     Route::get('/', 'DashboardController@Home');
     Route::resource('etablissements', 'EtablissementController');
     Route::resource('classe', 'ClasseController');
-    
+    //Route::resource('Noe')
 });

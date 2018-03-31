b392a734cabb3657556cf6ce5329233d7d46e15b
David H <david95thinkcode@gmail.com>
2018-03-31 14:08:53 +0100

    [ Replacement ] Coefficier devient Enseigner

---
+++ app/Models/Enseigner.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Enseigner extends Model
+{
+    protected $table = 'enseigner';
+    
+    public $timestamps = true;
+
+    /**
+     * Une matière est enseignée dans une classe
+     */
+    public function matiere()
+    {
+        return $this->belongsTo('App\Models\Matiere', 'matiere_id');
+    }
+
+    public function classe()
+    {
+        return $this->belongsTo('App\Models\Classe');
+    }
+
+    /**
+     * Une matière est enseignée dans une classe par un professeur
+     */
+    public function professeur()
+    {
+        return $this->belongsTo('App\Models\Professeur', 'professeur_id');
+    }
+}

0a34d6b3a7478789a88915f17a3149375a5c0633
David H <david95thinkcode@gmail.com>
2018-04-09 17:53:14 +0100

    [ Update ] Vue enrichie

--- resources/views/partials/session-messages.blade.php
+++ resources/views/partials/session-messages.blade.php
@@ -1,14 +1,30 @@
 @if (session('status'))
-<div class="alert alert-success text-center" role="alert">
-	{{ session('status') }}
-</div>
+	<div class="alert alert-success text-center" role="alert">
+		{{ session('status') }}
+	</div>
+
+@elseif (session('info'))
+	<div class="alert alert-info text-center" role="alert">
+		{{ session('info') }}
+	</div>
+
+@elseif (session('warning'))
+	<div class="alert alert-warning text-center" role="alert">
+		{{ session('warning') }}
+	</div>
+
+@elseif (session('danger'))
+	<div class="alert alert-danger text-center" role="alert">
+		{{ session('danger') }}
+	</div>
+
 @elseif ($errors->any())
-<div class="alert alert-danger" role="alert">
-	<ul>
-		@foreach ($errors->all() as $error)
-		<li>{{ $error }}</li>
-		@endforeach
-	</ul>
-</div>
+	<div class="alert alert-danger" role="alert">
+		<ul>
+			@foreach ($errors->all() as $error)
+			<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
 @else
 @endif

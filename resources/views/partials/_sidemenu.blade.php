943aac649a726beb85b4268f100ad88c371d36ea
David H <david95thinkcode@gmail.com>
2018-04-10 15:35:40 +0100

    [ Update ] Integration in progress...

---
+++ resources/views/partials/_sidemenu.blade.php
@@ -0,0 +1,74 @@
+<div class="col-md-3 left_col menu_fixed">
+    <div class="left_col scroll-view">
+        <div class="navbar nav_title mb-1" style="border: 0;">
+            <a href="index.html" class="site_title">
+                <i class="fa fa-graduation-cap"></i> 
+                <span>AfriSchool</span>
+            </a>
+        </div>
+
+        <!-- sidebar menu -->
+        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+        <div class="menu_section">
+            <h3>General</h3>
+            <ul class="nav side-menu">
+                <li><a><i class="fa fa-users"></i> Élèves 
+                    <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="#">Créer</a></li>
+                        <li><a href="#">...........</a></li>
+                        <li><a href="#">...........</a></li>
+                    </ul>
+                </li>
+                <li><a><i class="fa fa-user-secret"></i> Professeurs <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="#">Créer</a></li>
+                        <li><a href="#">...........</a></li>
+                        <li><a href="#">...........</a></li>
+                    </ul>
+                </li>
+                <li><a><i class="fa fa-street-view"></i>Personnel <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="#">Créer</a></li>
+                        <li><a href="#">...........</a></li>
+                        <li><a href="#">...........</a></li>
+                    </ul>
+                </li>
+                <li><a><i class="fa fa-building-o"></i> Classe <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="#">Créer</a></li>
+                        <li><a href="#">...........</a></li>
+                        <li><a href="#">...........</a></li>
+                    </ul>
+                </li>
+                <li class=""><a><i class="fa fa-money "></i> Finance 
+                    <span class="label label-success pull-right">Coming Soon</span></a>
+                    
+                </li>
+                <li><a><i class="fa fa-hashtag"></i> Absence 
+                    <span class="label label-success pull-right">Coming Soon</span></span></a>
+                
+                </li>
+            </ul>
+        </div>
+        </div>
+        <!-- /sidebar menu -->
+
+        <!-- /menu footer buttons -->
+        <div class="sidebar-footer hidden-small">
+        <a data-toggle="tooltip" data-placement="top" title="Paramètre">
+            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+        </a>
+        <a data-toggle="tooltip" data-placement="top" title="Plein écran">
+            <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+        </a>
+        <a data-toggle="tooltip" data-placement="top" title="Lock">
+            <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+        </a>
+        <a data-toggle="tooltip" data-placement="top" title="Deconnexion" href="login.html">
+            <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+        </a>
+        </div>
+        <!-- /menu footer buttons -->
+    </div>
+</div>
---
+++ resources/views/templates/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui" />
+    <title>@yield('title') | Afrischool</title>
+    <link rel="icon" href="#">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="{{mix('css/app.css')}}" rel="stylesheet"> 
+    <link href="{{asset('css/jquery.mCustomScrollbar.min.css')}}" rel="stylesheet"> 
+    <link href="{{asset('css/custom.min.css')}}" rel="stylesheet"> 
+    @yield('css')
+    <link href="{{asset('css/ie10-viewport-bug-workaround.css')}}" rel="stylesheet"> 
+    <!--[if IE]>
+        <link href="{{asset('css/bootstrap-ie9.css')}}" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>   
+    <![endif]-->
+    <!--[if lt IE 9]>
+        <link href="{{asset('css/bootstrap-ie8.css')}}" rel="stylesheet">
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="nav-md">
+  <div class="container body">
+    <div class="main_container">
+        @include('partials._sidemenu')
+
+        <!-- top navigation -->
+        @include('partials._topnav')
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+            <div class="">
+                @yield('content')
+            </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        @include('partials.footer')
+        <!-- /footer content -->
+    </div>
+  </div>
+    
+    <!-- Scripts -->
+    <script src="{{mix('js/app.js')}}"></script>
+    <script src="{{asset('js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script src="{{asset('js/custom.min.js')}}"></script>
+    @yield('js')
+    <script src="{{asset('js/ie10-viewport-bug-workaround.js')}}"></script>
+</body>
+
+</html>

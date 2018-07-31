c11d7d45b3eb7f8db6ca6f10cbc1da75c0284945
David K. Hiheaglo <davidhihea@gmail.com>
2018-07-31 01:32:37 +0100

    [ Update ] Beautifying code

--- app/Http/Controllers/BulletinController.php
+++ app/Http/Controllers/BulletinController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\StoreCriteresBulletinRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCriteresBulletinRequest;
 use App\Models\AnneeScolaire;
 use App\Models\Classe;
 use App\Models\Enseigner;
 use App\Models\Inscription;
-use App\Models\Trimestre;
-use App\Models\Note;
 use App\Models\Matiere;
+use App\Models\Note;
+use App\Models\Trimestre;
 
 class BulletinController extends Controller
 {
@@ -43,31 +42,30 @@ class BulletinController extends Controller
         session()->put('niveau', $niveau);
         $years = AnneeScolaire::all();
         $classes = Classe::where($attribute, true)->get();
-        
+
         return view('dashboard.bulletins.step-2', compact('years', 'classes'));
     }
 
     public function ListEleves(StoreCriteresBulletinRequest $req)
     {
         $niveau = session()->pull('niveau'); //netoyage de la variable de session
-        
+
         $trimestres = Trimestre::all();
         $year = AnneeScolaire::findOrFail($req->anneeScolaire)->toArray();
         $classe = Classe::findOrFail($req->classe)->toArray();
-        session()->put('anneescolaire', $year);        
+        session()->put('anneescolaire', $year);
         session()->put('classe', $classe);
-        
+
         $eleves = Inscription::with('eleve')
             ->where([
                 ['classe_id', $req->classe],
-                ['annee_scolaire_id', $req->anneeScolaire]
+                ['annee_scolaire_id', $req->anneeScolaire],
             ])
             ->get();
 
         return view('dashboard.bulletins.step-3', compact('eleves', 'trimestres'));
     }
 
-    
     public function ShowByTrimestre($idTrimestre, $matricule)
     {
         // le matricule est le numéro de l'élève dans la table inscription
@@ -84,11 +82,11 @@ class BulletinController extends Controller
         if ($eleve != null) {
             // matière enseignées dans la classe
             $matEnseignees = Enseigner::with('matiere', 'professeur')
-            ->where([
-                ['classe_id', $eleve->classe_id],
-                ['annee_scolaire_id', session()->get('anneescolaire.id')]
-            ])
-            ->get();
+                ->where([
+                    ['classe_id', $eleve->classe_id],
+                    ['annee_scolaire_id', session()->get('anneescolaire.id')],
+                ])
+                ->get();
 
             // les notes de chaque matière
             foreach ($matEnseignees as $key => $m) {
@@ -97,31 +95,38 @@ class BulletinController extends Controller
                     ['eleve_id', $eleve->eleve_id],
                     ['classe_id', $eleve->classe_id],
                     ['trimestre_id', $idTrimestre],
-                    ['matiere_id', $m['matiere_id']]
+                    ['matiere_id', $m['matiere_id']],
                 ])
-                ->get();
-                
-                array_push($notesBrutes, $note);
+                    ->get();
+
+                if (count($note) > 0) {
+                    array_push($notesBrutes, $note);
+                }
             }
 
-            $notesOrdonnes = $this->OrdonnerNotes($notesBrutes, $matEnseignees);
-            
-            // dd($notesOrdonnes);
-            
-            // Montrons la vue adéquate
-            if (session()->get('classe.estPrimaire') == 1) {
-                $view = 'dashboard.bulletins.b-primaire';
-            }
-            else if (session()->get('classe.estCollege') == 1) {
-                $view = 'dashboard.bulletins.b-college';            }
+            if (count($notesBrutes) > 0) {
+                $notesOrdonnes = $this->OrdonnerNotes($notesBrutes, $matEnseignees);
+                // dd($notesOrdonnes);
 
-            return view($view, compact('eleve','notesOrdonnes'));
-        }
-        else {
+                // Montrons la vue adéquate
+                if (session()->get('classe.estPrimaire') == 1) {
+                    $view = 'dashboard.bulletins.b-primaire';
+                } else if (session()->get('classe.estCollege') == 1) {
+                    $view = 'dashboard.bulletins.b-college';
+                }
+
+                return view($view, compact('eleve', 'notesOrdonnes'));
+
+            } else {
+                // TODO pour @Romeo 
+                // faire une vue pour afficher le message ci-dessous dedans
+                return ('Impossible de générer le bulletin car aucune note enregistrée pour ce trimestre');
+            }
+
+        } else {
             abort(404);
         }
     }
-    
 
     private function OrdonnerNotes($notesBruteArray, $EnseignerArrays)
     {
@@ -133,11 +138,11 @@ class BulletinController extends Controller
 
         // Remplissage $distinctsMatieres
         foreach ($EnseignerArrays as $key => $e) {
-            
+
             if (count($distinctsMatieres) == 0) {
                 array_push($distinctsMatieres, $e['matiere_id']);
             } else {
-                
+
                 $found = false;
                 foreach ($distinctsMatieres as $matiereID) {
                     if ($e['matiere_id'] == $matiereID) {
@@ -146,20 +151,20 @@ class BulletinController extends Controller
                 }
                 if (!$found) {
                     array_push($distinctsMatieres, $e['matiere_id']);
-                }                
+                }
             }
         }
-
+        
         // ordering notes
         // chaque collection de note de notesbrutes
         foreach ($notesBruteArray as $key => $noteCollection) {
-            
+
             $matiereID;
             $matiere = Matiere::findOrFail($matiereID);
             $classifiedNoteByMatiere = [
                 'interrogation' => [],
                 'devoir' => [],
-                'examen' => []
+                'examen' => [],
             ];
 
             /** structuration des notes d'une matière dans un tableau
@@ -170,11 +175,11 @@ class BulletinController extends Controller
              *                          [ 'note_not' => 11 ],
              *                        ]
              * ]
-             */ 
-            foreach ($noteCollection as $k => $v) {  
-                
+             */
+            foreach ($noteCollection as $k => $v) {
+
                 $matiereID = $v['matiere_id'];
-                
+
                 switch ($v['types_evaluation_id']) {
                     case 1:
                         array_push($classifiedNoteByMatiere['interrogation'], $v);
@@ -191,13 +196,11 @@ class BulletinController extends Controller
                 }
             }
 
-
             // Détails de la amtière
             $matiereDetails;
 
             foreach ($EnseignerArrays as $ensIndex => $enseignerModel) {
-                if ($enseignerModel->matiere->id == $matiere->id) 
-                {
+                if ($enseignerModel->matiere->id == $matiere->id) {
                     $matiereDetails = $enseignerModel;
                     // echo("Trouvé : " . $enseignerModel->matiere->id . " == " . $matiereID . " - " );
                     // echo ($matiereDetails->matiere->intitule . ' : ' .$matiereDetails->professeur->prof_nom . ' ' . $matiereDetails->professeur->prof_prenoms . "\n");
@@ -211,5 +214,5 @@ class BulletinController extends Controller
 
         return $orderedNotes;
     }
-    
+
 }

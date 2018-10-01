6c929b25bdfabb57d14ab39fe24a1bba00137f8b
Roméo GBONGBON <g.meo@outlook.com>
2018-10-01 10:44:11 +0100

    upadte app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,5 +5,17 @@
  */
 
 require('./bootstrap');
-require('./custom');
-require('./textwriting');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const app = new Vue({
+    el: '#app'
+});

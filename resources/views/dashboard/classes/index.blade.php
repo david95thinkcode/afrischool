5cff3a66ffb04326c430e91fb9eb84d1205734a0
David K. Hiheaglo <davidhihea@gmail.com>
2018-10-02 12:16:02 +0100

    [ Update ] La partie "université" a été retirée

--- resources/views/dashboard/classes/index.blade.php
+++ resources/views/dashboard/classes/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class='row'>
     <a href="{!! route('classe.list', ['niveau' => 'PRM']) !!}" title='Cliquez pour voir la liste complète'>
-        <div class="animated flipInY col-sm-offset-2 col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="animated flipInY col-sm-offset-2 col-lg-4 col-md-4 col-sm-4 col-xs-12">
             <div class="tile-stats bg-green">
                 <div class="count">{!! $numberOfClasses['PRM'] !!}</div>
                 <h3>Primaire</h3>
@@ -13,19 +13,10 @@
     </a>
 
     <a href="{!! route('classe.list', ['niveau' => 'CLG']) !!}" title='Cliquez pour voir la liste complète'>
-        <div class="animated flipInY col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="animated flipInY col-lg-4 col-md-4 col-sm-4 col-xs-12">
             <div class="tile-stats bg-green">
                 <div class="count">{!! $numberOfClasses['CLG'] !!}</div>
                 <h3>Collège</h3>
-            </div>
-        </div>
-    </a>
-
-    <a href="{!! route('classe.list', ['niveau' => 'UNV']) !!}" title='Cliquez pour voir la liste complète'>
-        <div class="animated flipInY col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <div class="tile-stats bg-green">
-                <div class="count">{!! $numberOfClasses['UNV'] !!}</div>
-                <h3>Université</h3>
             </div>
         </div>
     </a>
